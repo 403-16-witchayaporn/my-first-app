4456a0ff135efb297281f8b1ecc164de889efea1
403-16-witchayaporn <54523@yupparaj.ac.th>
2026-08-19 20:31:17 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -69,7 +69,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         )
 
     # ตรวจข้อ 4
-    if u_ans3 == "Coconut ":
+    if u_ans4 == "pen ":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
